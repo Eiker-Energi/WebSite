242f10d9cefe137b8f458b9f57ee3b1554b742ce
Eiker-Energi <nazar@oeenergi.no>
2025-11-10 15:56:54 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,19 @@
 /** @type {import('next').NextConfig} */
+const isGhActions = process.env.GITHUB_ACTIONS === 'true';
+const repo = process.env.GITHUB_REPOSITORY
+  ? process.env.GITHUB_REPOSITORY.split('/')[1]
+  : '';
+
+const ghPagesOpts = isGhActions && repo
+  ? { basePath: `/${repo}`, assetPrefix: `/${repo}/` }
+  : {};
+
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
   trailingSlash: true,
-  assetPrefix: 'https://',
   experimental: { typedRoutes: true },
+  ...ghPagesOpts,
 };
+
 module.exports = nextConfig;
